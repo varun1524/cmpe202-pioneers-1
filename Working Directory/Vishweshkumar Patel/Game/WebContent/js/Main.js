3bd36a65121e7f03c91f12180a2b03a388805cda
Vishwesh <Vishwesh.carrier@outlook.com>
2017-10-25 23:59:18 -0700

    working on images,spritesheet and atlas

--- Working Directory/Vishweshkumar Patel/Game/WebContent/js/Main.js
+++ Working Directory/Vishweshkumar Patel/Game/WebContent/js/Main.js
@@ -1,11 +1,4 @@
 window.onload = function() {
-	var game = new Phaser.Game(800, 600, Phaser.AUTO);
-
-	// Add the States your game has.
-	// game.state.add("Boot", Boot);
-	// game.state.add("Menu", Menu);
-	// game.state.add("Preload", Preload);
-	game.state.add("Level", Level);
-
-	game.state.start("Level");
+	var game = new Phaser.Game(1200, 800, Phaser.AUTO);
+	game.state.add("Level1", Level1, true);
 };
