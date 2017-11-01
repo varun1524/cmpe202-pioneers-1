1d66093a9ced6bf9890d3eff49987b5b89165e99
Vishwesh <Vishwesh.carrier@outlook.com>
2017-10-31 23:59:23 -0700

    Updated Scene1 for Level 1, added sprites to their initial positions

--- Working Directory/Vishweshkumar Patel/Game/WebContent/assets/scenes/Scene1.js
+++ Working Directory/Vishweshkumar Patel/Game/WebContent/assets/scenes/Scene1.js
@@ -12,6 +12,22 @@ function Scene1(aGame, aParent) {
 
 	/* --- pre-init-end --- */
 
+	//settling background
+	var BG = this.game.add.sprite( x, y, 'BG', null, this);
+	BG.scale.setTo(1.2388787593597599, 1.1360282940928768);
+
+	//player sprite
+	this.game.add.sprite( x, y, 'player', 0, this);
+
+	//tile sprite: on which player will move around
+	this.game.add.tileSprite(416, 598, 384, 93, 'tiles', '14', this);
+
+	//part of tile sprite 
+	this.game.add.sprite(288, 598, 'tiles', '13', this);
+
+	//part of tile sprite 
+	this.game.add.sprite(800, 598, 'tiles', '15', this);
+	
 	/* --- post-init-begin --- */
 
 	/* --- post-init-end --- */
